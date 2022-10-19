80a652facbde6cccd7613c78899922437d8a6630
Ioan Botez <i.botez@student.rug.nl>
2022-10-19 17:50:07 +0200

    First steps towards database

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, dev::Server, get, post, App, HttpResponse, HttpServer, Responder};
+
+#[derive(serde::Deserialize)]
+struct SubscriptionData {
+    name: String,
+    email: String
+}
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[post("/subscriptions")]
+async fn subscribe(form: web::Form<SubscriptionData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check))
+    let server = HttpServer::new(|| App::new().service(health_check).service(subscribe))
         .listen(listener)?
         .run();
     Ok(server)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -28,22 +28,21 @@ async fn health_check_test() {
 }
 
 #[tokio::test]
-pub async fn subscribe_returns_200_for_valid_form() {
-    let address = spawn_app();
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
     let client = reqwest::Client::new();
-
-    let body = "name=le%20guin&email=ursula_le_guin@40gmail.com";
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}/subscriptions", &address))
+        .post(&format!("{}/subscriptions", &app_address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
         .expect("Failed to execute request.");
-
-    assert!(response.status().is_success());
+    // Assert
     assert_eq!(200, response.status().as_u16());
-    assert_eq!(Some(0), response.content_length());
 }
 
 #[tokio::test]
